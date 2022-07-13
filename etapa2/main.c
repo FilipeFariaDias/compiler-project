bc9e1a7545c216cb7af8343fedd4d4ccc36b23c6
germano98 <56523658+germano98@users.noreply.github.com>
2022-07-12 21:14:46 -0300

    Add files via upload

---
+++ etapa2/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern FILE* yyin;
+extern FILE* file();
+extern int isRunning();
+extern int getLineNumber();
+extern int yyparse();
+extern void initMe();
+extern void hashPrint();
+
+int main(int argc, char *argv[]){
+
+    int token;
+	
+	if(argc < 2){
+		fprintf(stderr,"Call: ./etapa2 nomearquivo\n");
+		return 1;
+	}
+	if(!(file(argv[1]))){
+		fprintf(stderr,"Cannot open file %s\n",argv[1]);
+		return 2;
+	}
+	
+	initMe();
+	
+	yyparse();
+	
+	hashPrint();
+
+	fprintf(stderr, "OK\n");
+
+ return 0; 
+}
+
