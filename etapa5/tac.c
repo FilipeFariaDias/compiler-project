52380b398f7114865a7277eb0abda8769f43566e
FilipeFariaDias <filipefariadias@hotmail.com>
2022-09-04 16:46:17 -0300

    Add join and printbackwards

--- etapa5/tac.c
+++ etapa5/tac.c
@@ -10,10 +10,23 @@ void tacPrint(TAC *tac){
 
 }
 void tacPrintBackwards(TAC *tac){
-
+    if(tac == NULL)
+        return;
+    tacPrintBackwards(tac->prev);
+    tacPrint(tac);
 }
 TAC* tacJoin(TAC *l1, TAC *l2){
+    TAC *point;
 
+    if(l1 == NULL)
+        return l2;
+    if(l2 == NULL)
+        return l1;
+
+    for(point = l2; point->prev != 0; point = point->prev);
+
+    point->prev = l1;
+    return l2;
 }
 
 TAC* generateCode(AST *node){
