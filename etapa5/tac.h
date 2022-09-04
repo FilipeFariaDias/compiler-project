e26682ed2b2736aa6913944ab3840cd06d88cf9c
germano98 <56523658+germano98@users.noreply.github.com>
2022-09-04 20:38:24 -0300

    Update tac.h

--- etapa5/tac.h
+++ etapa5/tac.h
@@ -8,6 +8,32 @@
 #define TAC_SUM     2
 #define TAC_DEC     3
 
+/* Não sei se todos esses defines são necessários, conferir
+#define TAC_MOVE 4
+#define TAC_MUL 5
+#define TAC_DIV 6
+#define TAC_GREAT 7
+#define TAC_LESS 8
+#define TAC_GE 9
+#define TAC_LE 10
+#define TAC_EQ 11
+#define TAC_DIF 12
+#define TAC_AND 13
+#define TAC_OR 14
+#define TAC_NOT 15
+#define TAC_LABEL 16
+#define TAC_BEGINFUN 17
+#define TAC_ARGPUSH 18
+#define TAC_ENDFUN 19
+#define TAC_IFZ 20
+#define TAC_JUMP 21
+#define TAC_CALL 22
+#define TAC_ARG 23
+#define TAC_RET 24
+#define TAC_PRINT 25
+#define TAC_READ 26
+#define TAC_VEC 27
+*/
 
 typedef struct tac_node{
     int type;
